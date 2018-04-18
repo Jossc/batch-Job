e67f4a46e9e333b39d78c3f21f4ff7f3ffa24e3e
chenzhuo@wanhuchina.com <chenzhuo@wanhuchina.com>
2018-04-18 11:42:05 +0800

    添加模块 admin:管理端 core:工具类 job:任务调度执行

---
+++ src/main/java/com/batch/job/LogAspect/LogAspect.java
@@ -0,0 +1,16 @@
+package com.batch.job.LogAspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @PACKAGE_NAME: com.batch.job.LogAspect
+ * @PROJECT_NAME: batch-job
+ * @Date: 2018/4/18
+ * @author: chenzhuo
+ * 日志切面
+ */
+@Aspect
+@Component
+public class LogAspect {
+}
---
+++ src/main/java/com/batch/job/annotation/BatchAnnotation.java
@@ -0,0 +1,11 @@
+package com.batch.job.annotation;
+
+/**
+ * @PACKAGE_NAME: com.batch.job.annotation
+ * @PROJECT_NAME: batch-job
+ * @Date: 2018/4/18
+ * @author: chenzhuo
+ * 自定义注解方便重复使用
+ */
+public @interface BatchAnnotation {
+}
