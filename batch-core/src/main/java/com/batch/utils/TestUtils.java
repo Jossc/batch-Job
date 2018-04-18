4ce7a208246b080a1d2eb186b4bfca543132d807
chenzhuo@wanhuchina.com <chenzhuo@wanhuchina.com>
2018-04-18 14:56:52 +0800

    目录构建完成，

---
+++ batch-core/src/main/java/com/batch/utils/TestUtils.java
@@ -0,0 +1,10 @@
+package com.batch.utils;
+
+/**
+ * @PACKAGE_NAME: com.batch.utils
+ * @PROJECT_NAME: batch-job
+ * @Date: 2018/4/18
+ * @author: chenzhuo
+ */
+public class TestUtils {
+}
---
+++ batch-executor/job-exector-spring/src/main/src/com/exector/sprng/run/Test.java
@@ -0,0 +1,10 @@
+package com.exector.sprng.run;
+
+/**
+ * @PACKAGE_NAME: com.exector.sprng.run
+ * @PROJECT_NAME: batch-job
+ * @Date: 2018/4/18
+ * @author: chenzhuo
+ */
+public class Test {
+}
